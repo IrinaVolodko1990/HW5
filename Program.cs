915d882fcb29d1666727f79c427451369157e397
IrinaVolodko <8232734@gmail.com>
2023-07-18 12:32:34 +0300

    First task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+int size = new Random().Next(5, 11);
+int[] array = new int[size];
+int count = 0;
+void fillArray(int[] arr)
+{
+    Console.Write("Some array: [ ");
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+
+int countOdd(int[] arr)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+   
+    return count;
+}
+fillArray(array);
+Console.WriteLine();
+Console.Write($"In array {countOdd(array)} odd numbers");
